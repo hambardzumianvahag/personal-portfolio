50efcae5995d2faaa5857abcd8dd650a2489be46
hambardzumian_v <hambardzumianvahag@gmail.com>
2024-08-13 19:47:35 +0400

    some changes with size of elements

--- src/script.js
+++ src/script.js
@@ -1,16 +1,15 @@
 const burgerMenuButton = document.getElementById("burger-menu");
 const menuIcon = document.getElementById("menu-icon");
 const mobileMenu = document.getElementById("mobile-menu");
-const profile = document.getElementById("profile");
+const nextIcon = document.getElementById("next-icon");
+const exIcon = document.getElementById("ex-icon");
 
 burgerMenuButton.addEventListener("click", function () {
   mobileMenu.classList.toggle("hidden");
   if (mobileMenu.classList.contains("hidden")) {
     menuIcon.src = "./src/img/burger-menu.png";
-    profile.style.paddingTop = "5px";
   } else {
     menuIcon.src = "./src/img/burger-close.png";
-    profile.style.paddingTop = "260px";
   }
 });
 
@@ -33,12 +32,18 @@ function toggleTheme() {
   if (currentTheme === "light") {
     htmlElement.classList.remove("light");
     htmlElement.classList.add("dark");
+    nextIcon.src = "./src/img/next-icon-light.png";
+    exIcon.src = "./src/img/ex-icon-light.png";
     document.getElementById("theme-icon").src = "./src/img/dark-icon.png";
-    document.getElementById("mobile-theme-icon").src = "./src/img/dark-icon.png";
+    document.getElementById("mobile-theme-icon").src =
+      "./src/img/dark-icon.png";
   } else {
     htmlElement.classList.remove("dark");
     htmlElement.classList.add("light");
+    nextIcon.src = "./src/img/next-icon-dark.png";
+    exIcon.src = "./src/img/ex-icon-dark.png";
     document.getElementById("theme-icon").src = "./src/img/light-icon.png";
-    document.getElementById("mobile-theme-icon").src = "./src/img/light-icon.png";
+    document.getElementById("mobile-theme-icon").src =
+      "./src/img/light-icon.png";
   }
 }
